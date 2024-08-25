3dcad2c224d2aceae9743d799a1900e4086a7482
jatin <jatinahirwar07@gmail.com>
2024-08-25 08:50:32 +0530

    upload images in editor

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import {Editor } from '@tinymce/tinymce-react';
 import {Controller } from 'react-hook-form';
 import conf from '../conf/conf';
+import appwriteService from "../appwrite/config";
+
+
+
+
 
 
 export default function RTE({name, control, label, defaultValue =""}) {
+
+// Function to handle the image upload
+const handleImageUpload = (blobInfo, progress) => new Promise(async (resolve, reject) => {
+
+  const file = blobInfo.blob(); // Get the file from blobInfo
+  const uploadedFile = await appwriteService.uploadFile(file);
+
+  const fileName = appwriteService.getFilePreview(uploadedFile.$id);
+  console.log(uploadedFile);
+  console.log("file name:: ",fileName);
+
+  resolve(fileName.href);
+});
+
   return (
     <div className='w-full'> 
     {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
@@ -42,6 +61,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
                 "wordcount",
                 "anchor",
             ],
+            images_upload_handler: handleImageUpload,
             toolbar:
             "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
             content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
